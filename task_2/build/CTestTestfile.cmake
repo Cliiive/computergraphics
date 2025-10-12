211ed82dd0d12063a51af2cd4c7488208a36a25b
jonas <sasowski@outlook.de>
2025-10-12 13:37:35 +0200

    Add files for Task2

---
+++ task_2/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: E:/Development/studium/computergraphics/task_2
+# Build directory: E:/Development/studium/computergraphics/task_2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
